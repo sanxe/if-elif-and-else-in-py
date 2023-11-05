52233f6e78908f9f326ab42ca3de491663b4175d
sanxe <146511000+sanxe@users.noreply.github.com>
2023-11-05 19:03:42 +0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,31 @@
+# num1 = float(input("please enter the first number\n"))
+# operator = input("please enter an operator\n")
+# num2 = float(input("please enter the second number\n"))
+
+# if operator == "+":
+#     print(num1+num2)
+# elif operator == "-":
+#     print(num1-num2)
+# elif operator == "*":
+#     print(num1*num2)
+# elif operator == "/":
+#     print(num1/num2)
+# else:
+#     print("invalid number or operator")
+
+username = ("admin")
+password = ("adminpassword123")
+
+input1 = input("please enter username:\n")
+input2 = input("please enter password:\n")
+if input1 == username and input2 == password:
+    print("login successful!")
+elif input1 != username or input2 != password:
+    print("wrong password or username")
+
+
+        
+    
+
+
+
